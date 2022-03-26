2023a8ce177b86d80166455edf02a929f94736a3
Emir Cokrlija <emir.compwiz@gmail.com>
2022-03-26 13:23:40 -0400

    added routes for goals

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,4 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "Get all goals" });
+});
+
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Set goal" });
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: "Update goal" });
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: "Delete goal" });
+});
+
 module.exports = router;
