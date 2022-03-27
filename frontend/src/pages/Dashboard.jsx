431bbb0e29d8a0667e4343992e6f5a01607c32f8
Emir Cokrlija <emir.compwiz@gmail.com>
2022-03-26 21:41:59 -0400

    create goal form

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import GoalForm from "../components/GoalForm";
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -20,6 +21,7 @@ function Dashboard() {
         <h1>Welcome {user && user.name}</h1>
         <p>Goals Dashboard</p>
       </section>
+      <GoalForm />
     </>
   );
 }
